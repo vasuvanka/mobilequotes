ebafbf216b15db985764021ae5c733996b081d12
vasuvanka <vanka.vasu@gmail.com>
2015-08-14 11:43:05 +0530

    update

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var http = require('http');
 var express = require('express');
 var app = express();
 var socket=require('socket.io');
-var server = http.createServer(app).listen(8088);
+var server = http.createServer(app).listen(3000);
 var io = socket.listen(server);
 app.use(bodyParser.urlencoded({extended : true}));
 app.use(bodyParser.json());
